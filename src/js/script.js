435ac8a01dc3ab852e6ee003ae1121632492e9dd
sasataka-c <osasa.t0705@gmail.com>
2023-08-26 13:27:02 +0900

    test

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,41 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    $('.js-hamburger').on('click', function () {
+        if ($('.js-hamburger').hasClass('is-active')) {
+            $('.js-hamburger').removeClass('is-active');
+            $('body').removeClass('is-fixed');
+            $('.js-sp-nav').fadeOut();
+            // $('.js-sp-nav').removeClass('is-active');
+        } else {
+            $('.js-hamburger').addClass('is-active');
+            $('body').addClass('is-fixed');
+            $('.js-sp-nav').fadeIn();
+            // $('.js-sp-nav').addClass('is-active');
+        }
+    });
+
+    const mvSwiper = new Swiper(".js-mv-swiper", {
+        // loop: true,
+        effect: 'fade',
+        speed: 1500,
+        // autoplay: {
+        //     delay: 3000,
+        // }
+    });
+
+    const campaignSwiper = new Swiper(".js-campaign-Swiper", {
+        slidesPerView: 'auto',
+        loop: true,
+        spaceBetween: 16,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        // autoplay: {
+        //     delay: 3000,
+        // },
+    });
 
 });
