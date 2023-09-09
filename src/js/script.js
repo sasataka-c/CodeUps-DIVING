bb1f6c02860fc80ad2513289ace907cf3518f216
sasataka-c <osasa.t0705@gmail.com>
2023-09-10 05:49:01 +0900

    test

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    // ドロワーメニュー
     $('.js-hamburger').on('click', function () {
         if ($('.js-hamburger').hasClass('is-active')) {
             $('.js-hamburger').removeClass('is-active');
@@ -15,6 +16,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
     });
 
+    // メインビュー画像スライダー
     const mvSwiper = new Swiper(".js-mv-swiper", {
         // loop: true,
         effect: 'fade',
@@ -24,18 +26,98 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         // }
     });
 
-    const campaignSwiper = new Swiper(".js-campaign-Swiper", {
-        slidesPerView: 'auto',
+    // キャンペーン画像スライダー
+    const campaignSwiper = new Swiper(".js-campaign-swiper", {
         loop: true,
-        spaceBetween: 16,
-        grabCursor: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
+        spaceBetween: 24,
+        speed: 2000,
+        width: 280,
+        loopedSlides: 8,
         // autoplay: {
-        //     delay: 3000,
+        //   delay: 2000,
+        //   disableOnInteraction: false,
         // },
+        breakpoints: {
+            768: {
+                spaceBetween: 40,
+                width: 333,
+            },
+        },
+
+        // 前後の矢印
+        navigation: {
+            nextEl: ".js-swiper-button-next",
+            prevEl: ".js-swiper-button-prev",
+        },
     });
+
+    // 画像のinveiw
+    let box = $(".js-colorbox"),
+        speed = 700;
+
+    //.colorboxの付いた全ての要素に対して下記の処理を行う
+    box.each(function () {
+        $(this).append('<div class="color"></div>');
+        var color = $(this).find($(".color")),
+            image = $(this).find("img");
+        var counter = 0;
+
+        image.css("opacity", "0");
+        color.css("width", "0%");
+        //inviewを使って背景色が画面に現れたら処理をする
+        color.on("inview", function () {
+            if (counter == 0) {
+                $(this)
+                    .delay(200)
+                    .animate({ width: "100%" }, speed, function () {
+                        image.css("opacity", "1");
+                        $(this).css({ left: "0", right: "auto" });
+                        $(this).animate({ width: "0%" }, speed);
+                    });
+                counter = 1;
+            }
+        });
+    });
+
+    //トップに戻るボタン
+  var topBtn = $(".js-totop");
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    var scrollPosition = $(this).scrollTop();
+    var windowHeight = $(this).height();
+    var bodyHeight = $(document).height();
+
+    // フッターの高さを動的に取得
+    var footerHeight = $(".footer").outerHeight() + 16;
+
+    if (scrollPosition > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+
+    // フッター手前でボタンを止める
+    if (bodyHeight - scrollPosition <= windowHeight + footerHeight) {
+      topBtn.css({ position: "absolute", bottom: footerHeight + "px" });
+    } else {
+      topBtn.css({ position: "fixed", bottom: "0px" });
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
 
 });
